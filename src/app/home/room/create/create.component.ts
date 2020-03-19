8019ceb8420af18825092cab010d6fddd906fedc
minhcuong5920 <vuminhcuong.5920@gmail.com>
2020-03-19 16:29:11 +0700

    ckeditor form va vadidate form

--- src/app/home/room/create/create.component.ts
+++ src/app/home/room/create/create.component.ts
@@ -1,15 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, Validators } from "@angular/forms";
+import { Router } from "@angular/router";
+import { IRoom } from "../../../interface/i-room";
+import { RoomService } from "src/app/service/room.service";
 
 @Component({
-  selector: 'app-create',
-  templateUrl: './create.component.html',
-  styleUrls: ['./create.component.css']
+  selector: "app-create",
+  templateUrl: "./create.component.html",
+  styleUrls: ["./create.component.css"]
 })
 export class CreateComponent implements OnInit {
+  model: IRoom;
+  formCreateRoom;
+  description: string = "";
 
-  constructor() { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
+    this.formCreateRoom = this.formBuilder.group({
+      name: ["", [Validators.required, Validators.minLength(6)]],
+      type_house: ["", [Validators.required]],
+      type_room: ["", [Validators.required]],
+      address: ["", [Validators.required]],
+      num_bed: ["", [Validators.required]],
+      num_bath: ["", [Validators.required]],
+      description: ["", [Validators.required]],
+      price: ["", [Validators.required]]
+    });
   }
 
+  onSubmit(data) {
+    this.model = data;
+    // this.roomService.create(this.model).subscribe(
+    //   (result: any) => {
+
+    //   });
+    console.log(this.model);
+  }
 }
--- src/app/service/room.service.ts
+++ src/app/service/room.service.ts
@@ -1,9 +1,40 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { IRoom } from '../interface/i-room';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RoomService {
 
-  constructor() { }
+  private url = '';
+  rooms: any;
+
+  constructor(private httpClient: HttpClient,
+              private route: Router) {
+  }
+
+ 
+  getAll(): Observable<IRoom[]> {
+    return this.httpClient.get<IRoom[]>(this.url + '/rooms');
+  }
+
+  getById(id: number) {
+    return this.httpClient.get(this.url + '/rooms/' + id);
+  }
+
+  create(user: IRoom) {
+    return this.httpClient.post(this.url + '/rooms', user);
+  }
+
+  update(user: IRoom, id) {
+    return this.httpClient.put(this.url + '/rooms/' + id, user);
+  }
+
+  delete(id: number) {
+    return this.httpClient.delete(this.url + '/rooms/' + id);
+  }
+
 }

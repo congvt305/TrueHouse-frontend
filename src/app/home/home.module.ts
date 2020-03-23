776971800e6d2d3e7bd21ba0a86bb8ab8fd2eeed
minhcuong5920 <vuminhcuong.5920@gmail.com>
2020-03-24 01:00:35 +0700

    chuc nang update anh tam on lay duoc id house muon up anh

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -6,19 +6,24 @@ import { FooterComponent } from './footer/footer.component';
 import { SearchComponent } from './search/search.component';
 import { AppRoutingModule } from '../app-routing.module';
 import { RoomComponent } from './room/room.component';
+import { FileUploadModule } from 'ng2-file-upload';
 import { CreateComponent } from './room/create/create.component';
 import { EditComponent } from './room/edit/edit.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { CKEditorModule } from 'ngx-ckeditor';
+import { RouterModule } from '@angular/router';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+
 
 
 
 @NgModule({
   declarations: [HomeComponent, HeaderComponent, FooterComponent, SearchComponent, RoomComponent, CreateComponent, EditComponent, NavbarComponent],
   imports: [
+    FileUploadModule,
     CommonModule,
+    RouterModule,
     AppRoutingModule,
     CKEditorModule,
     FormsModule,
--- src/app/home/room/create/create.component.ts
+++ src/app/home/room/create/create.component.ts
@@ -1,8 +1,11 @@
 import { Component, OnInit } from "@angular/core";
-import { FormBuilder, Validators } from "@angular/forms";
+import { FormBuilder, Validators, FormGroup } from "@angular/forms";
 import { Router } from "@angular/router";
 import { IRoom } from "../../../interface/i-room";
-import { RoomService } from '../../../service/room.service';
+import { RoomService } from "../../../service/room.service";
+import { FileUploader } from "ng2-file-upload";
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
 
 @Component({
   selector: "app-create",
@@ -13,15 +16,52 @@ export class CreateComponent implements OnInit {
   model: IRoom;
   formCreateRoom;
   description: string = "";
+  uploadForm: FormGroup;
+
+  uploader: FileUploader = new FileUploader({
+    isHTML5: true
+  });
+
+  ERROR_MESSAGE = {
+    name_house: [
+      {type: 'required', message: 'User is required.'},
+      {type: 'minlength', message: 'Username has min length : 6'},
+    ],
+    house_type: [
+      {type: 'required', message: 'House type is required.'},
+    ],
+    room_type: [
+      {type: 'required', message: 'Room type is required.'},
+    ],
+    address: [
+      {type: 'required', message: 'Address is required.'},
+    ],
+    bed_room_num: [
+      {type: 'required', message: 'Bed room num is required.'},
+    ],
+    bath_room_num: [
+      {type: 'required', message: 'Bath room num is required.'},
+    ],
+    description: [
+      {type: 'required', message: 'Description is required.'},
+    ],
+    price: [
+      {type: 'required', message: 'Price type is required.'},
+    ],
+    status: [
+      {type: 'required', message: 'House type is required.'},
+    ],
+  };
 
   constructor(
-    private formBuilder: FormBuilder,
+    private fb: FormBuilder,
     private router: Router,
-    private roomService: RoomService
+    private roomService: RoomService,
+    private http: HttpClient
   ) {}
 
   ngOnInit(): void {
-    this.formCreateRoom = this.formBuilder.group({
+    this.formCreateRoom = this.fb.group({
       name_house: ["", [Validators.required, Validators.minLength(6)]],
       house_type: ["", [Validators.required]],
       room_type: ["", [Validators.required]],
@@ -31,17 +71,44 @@ export class CreateComponent implements OnInit {
       description: ["", [Validators.required]],
       price: ["", [Validators.required]],
       user_id: [""],
-      status: [""]
+      status: ["",[Validators.required]],
+      document: [null, null],
+      type: [null, Validators.compose([Validators.required])]
     });
   }
 
   onSubmit(data) {
     this.model = data;
-    console.log(this.model);
-    
-    this.roomService.create(this.model).subscribe(
-      (result: any) => {
-        console.log(result);
-      });
+    this.roomService.create(this.model).subscribe((result: any) => {
+      console.log(result["data"].id);
+      let house_id = result["data"].id;
+
+      for (let i = 0; i < this.uploader.queue.length; i++) {
+        let fileItem = this.uploader.queue[i]._file;
+        if (fileItem.size > 10000000) {
+          alert("Each File should be less than 10 MB of size.");
+          return;
+        }
+      }
+
+      for (let j = 0; j < this.uploader.queue.length; j++) {
+        let data = new FormData();
+        let fileItem = this.uploader.queue[j]._file;
+        console.log(fileItem);
+        data.append("file", fileItem);
+        data.append("fileSeq", "seq" + j);
+        this.uploadFile(data, house_id).subscribe(data => {
+          console.log(data);
+        });
+      }
+      this.uploader.clearQueue();
+    });
+  }
+
+  uploadFile(data: FormData, house_id) {
+    return this.http.post(
+      "http://localhost:8000/api/multiple-image/" + house_id,
+      data
+    );
   }
 }

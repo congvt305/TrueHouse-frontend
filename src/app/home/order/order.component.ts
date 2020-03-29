f55ce36cc88f53710c2b19f4c191342c153c4067
minhcuong5920 <vuminhcuong.5920@gmail.com>
2020-03-29 16:50:43 +0700

    create duoc order

--- src/app/home/order/order.component.ts
+++ src/app/home/order/order.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { IOrder } from '../../interface/i-order';
+import { OrderService } from '../../service/order.service';
+import { Component, OnInit, Input} from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-order',
@@ -7,9 +11,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrderComponent implements OnInit {
 
-  constructor() { }
+  selected: { start: Date, end: Date };
+  formCreateOrder;
+  @Input() house_id: string;
+  checkin: number ;
+  model: IOrder;
+
+  constructor(
+    private fb: FormBuilder,
+    private router: Router,
+    private orderService: OrderService
+    ) { }
 
   ngOnInit(): void {
+    this.formCreateOrder = this.fb.group({
+      customer_name: ["", [Validators.required]],
+      customer_phone: ["", [Validators.required]],
+      house_id: [this.house_id],
+      user_id: [sessionStorage.getItem("token")],
+      date: ["", [Validators.required]]
+    });
   }
 
+  onSubmit(data) {
+    this.model = data;
+    this.model['checkin'] = data.date.start.format();
+    this.model['checkout'] = data.date.end.format();
+    this.model['totalPrice'] = '100000';
+
+    this.orderService.create(this.model).subscribe((result: any) => {
+      this.router.navigate(["/"]);
+    });
+
+    this.checkin = data.date.start.format();
+    console.log(this.checkin);    
+  }
 }
